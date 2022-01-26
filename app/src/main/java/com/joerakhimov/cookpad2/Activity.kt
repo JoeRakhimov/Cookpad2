903d38ff3229a4e0cf2340c04cdf174b837fc550
Gayrat Rakhimov <rakhimov.gayrat@gmail.com>
2022-01-26 16:01:17 +0100

    Set up navigation graph

---
+++ app/src/main/java/com/joerakhimov/cookpad2/Activity.kt
@@ -0,0 +1,10 @@
+package com.joerakhimov.cookpad2
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Activity : AppCompatActivity(R.layout.activity) {
+
+
+
+}
---
+++ app/src/main/java/com/joerakhimov/cookpad2/screen/main/MainFragment.kt
@@ -0,0 +1,8 @@
+package com.joerakhimov.cookpad2.screen.main
+
+import androidx.fragment.app.Fragment
+import com.joerakhimov.cookpad2.R
+
+class MainFragment: Fragment(R.layout.fragment_main) {
+
+}
